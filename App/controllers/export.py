1e7e4e5f54ecdb092f3a17647dc9fd72021c5c60
jeremy87lee <jeremylovell2004@gmail.com>
2026-07-29 10:45:13 -0400

    fixed export

--- App/controllers/export.py
+++ App/controllers/export.py
@@ -13,31 +13,60 @@ from io import BytesIO
 
 def export_excel():
     gates = Gate.query.all()
+    flights = Flight.query.all()
+    gates_2 = []
     wb = Workbook()
     ws = wb.active
     ws.title = "Flights"
     
     ws.append([
-        "Gate ID", "Terminal", "Flight ID", "Departure Destination", "Destination", "Plane ID", "Plane Model",
-        "Plane Capacity", "Pilot ID", "Pilot Name", "Departure Time", "Arrival Time"
+        "Flight ID", "Departure Destination", "Destination", "Plane ID", "Plane Model",
+        "Plane Capacity", "Pilot ID", "Pilot Name", "Departure Time", "Arrival Time", "Gate ID", "Gate Terminal"
     ])
+    
+    
 
     # Data rows
-    for g in gates:
-        ws.append([
-            g.id,
-            g.terminal,
-            g.flights.id if g.flights else "N/A",
-            g.flights.departure_destination if g.flights else "N/A",
-            g.flights.destination if g.flights else "N/A",
-            g.flights.plane.id if g.flights else "N/A",
-            g.flights.plane.model if g.flights else "N/A",
-            g.flights.plane.capacity if g.flights else "N/A",
-            g.flights.pilot.id if g.flights else "N/A",
-            g.flights.pilot.name if g.flights else "N/A",
-            g.flights.departure_time if g.flights else "N/A",
-            g.flights.arrival_time if g.flights else "N/A"
-        ]) 
+    for f in flights:
+        
+        for g in gates:
+            if g.flight == f.id:
+                gates_2.append(g)
+        
+        if gates_2:
+            for g in gates_2:   
+                ws.append([
+                    f.id if g.flights else "N/A",
+                    f.departure_destination if g.flights else "N/A",
+                    f.destination if g.flights else "N/A",
+                    f.plane.id if g.flights else "N/A",
+                    f.plane.model if g.flights else "N/A",
+                    f.plane.capacity if g.flights else "N/A",
+                    f.pilot.id if g.flights else "N/A",
+                    f.pilot.name if g.flights else "N/A",
+                    f.departure_time if g.flights else "N/A",
+                    f.arrival_time if g.flights else "N/A",
+                    g.id if g.id else "N/A",
+                    g.terminal if g.terminal else "N/A"
+                ]) 
+                gates_2.remove(g)
+        else:
+            ws.append([
+                                f.id if g.flights else "N/A",
+                                f.departure_destination if g.flights else "N/A",
+                                f.destination if g.flights else "N/A",
+                                f.plane.id if g.flights else "N/A",
+                                f.plane.model if g.flights else "N/A",
+                                f.plane.capacity if g.flights else "N/A",
+                                f.pilot.id if g.flights else "N/A",
+                                f.pilot.name if g.flights else "N/A",
+                                f.departure_time if g.flights else "N/A",
+                                f.arrival_time if g.flights else "N/A",
+                                "N/A",
+                                "N/A"
+                            ])
+        
+        
         
     # Save to an in-memory buffer instead of disk
     buffer = BytesIO()
